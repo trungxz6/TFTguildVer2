067bc0d0bbb6a132c53ac2c165cf4f0e7bd7d0ef
DINH THE CUONG <106668267+DinhTheCuong@users.noreply.github.com>
2023-10-04 20:59:23 +0700

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8000,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  optimizeDeps: {
+    include: ['tailwind.config.ts'],
+  },
 })
