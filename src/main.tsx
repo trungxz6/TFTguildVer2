5f46cc154d9d5fb11fca8a175fa5c0de876f53c7
DINH THE CUONG <106668267+DinhTheCuong@users.noreply.github.com>
2023-10-04 21:08:00 +0700

    setup font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './fonts/BeaufortforLOL-Regular.ttf'
+import './fonts/Spiegel_TT_Regular.ttf'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
