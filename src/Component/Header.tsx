03baa669f892d0cac6123fe579d0f1ed1a0a7a7e
bengxz <quoctrung061101@gmail.com>
2023-10-04 21:57:04 +0700

    ngay 1

--- src/Component/Header.tsx
+++ src/Component/Header.tsx
@@ -1,27 +1,53 @@
 import React from 'react'
-import { Select, Space } from 'antd';
+import { Select, ConfigProvider } from 'antd'
 
 const handleChange = (value: string) => {
-  console.log(`selected ${value}`);
-};
+  console.log(`selected ${value}`)
+}
+
+const { Option } = Select
+
+const Options = [
+  { value: '9.5', label: 'Set 9.5' },
+  { value: '10', label: 'Set 10' },
+  { value: '10.5', label: 'Set 10.5' },
+]
 
 const Header = () => {
+  // const renderOption = (option: any) => {
+  //   const { value, label } = option
+  //   return (
+  //     <div className={`${value === '9.5' ? 'text-red-500' : 'text-blue-500'} py-2 px-4 cursor-pointer m-[0px] p-[0px]`}>
+  //       {label}
+  //     </div>
+  //   )
+  // }
+
   return (
-    <div>
-        <Space wrap className='bg-blue-500'>
-            <Select
-            className=''
-            defaultValue="lucy"
-            style={{ width: 120 }}
-            onChange={handleChange}
-            options={[
-                { value: 'jack', label: 'Set 9.5' },
-                { value: 'lucy', label: 'Set 10' },
-                { value: 'Yiminghe', label: 'Set 10.5' },
-            ]}
-            />
-        </Space>
-    </div>
+    <ConfigProvider theme={{ components: { Select: { colorBgElevated: '#1F8FED', colorBgContainer: '#D67D68' } } }}>
+      <div className='bg-red-500 flex justify-center items-center m-[0px] p-[0px]'>
+        <img
+          className='brand w-[100px] h-[22px]'
+          src='https://rerollcdn.com/brand.svg'
+          alt='TFTactics'
+        ></img>
+        <Select
+          // className='bg-yellow-500 rounded-none m-[0px] p-[0px]'
+          // popupClassName='w-96 bg-white rounded-none hover:rounded-none m-[0px] p-[0px]'
+          defaultValue='Set 9.5'
+          onChange={handleChange}
+        >
+          {Options.map((option, index) => (
+            <Option
+              key={index}
+              value={option.value}
+            >
+              {/* {renderOption(option)} */}
+            </Option>
+          ))}
+        </Select>
+      </div>
+    </ConfigProvider>
   )
 }
 
