3c4b6a7baece59998e91356ba1381d209bef4bff
DINH THE CUONG <106668267+DinhTheCuong@users.noreply.github.com>
2023-11-01 08:24:56 +0700

    update 1.11

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './fonts/BeaufortforLOL-Regular.ttf'
 import './fonts/Spiegel_TT_Regular.ttf'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={new QueryClient()}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
